39818a731d4f553770c91d0b986ba1859997b3cb
Michael Fuchs <michfu@gmx.at>
2017-08-20 19:01:52 +0200

    translation

--- translations/harbour-weidenMonitor-de.ts
+++ translations/harbour-weidenMonitor-de.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name>CoverPage</name>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="42"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeit</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="47"/>
         <source>Wind</source>
-        <translation type="unfinished"></translation>
+        <translation>Wind</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="56"/>
         <source>Dir.</source>
-        <translation type="unfinished"></translation>
+        <translation>Richtg.</translation>
     </message>
 </context>
 <context>
@@ -24,42 +24,42 @@
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="15"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="19"/>
         <source>Reload</source>
-        <translation type="unfinished"></translation>
+        <translation>Neu laden</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="32"/>
         <source>Weiden am See</source>
-        <translation type="unfinished"></translation>
+        <translation>Weiden am See</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="78"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="86"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeit</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="91"/>
         <source>Windspeed/Gusts</source>
-        <translation type="unfinished"></translation>
+        <translation>Windgeschw./Böen</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="100"/>
         <source>Direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Richtung</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="107"/>
         <source>Windrose</source>
-        <translation type="unfinished"></translation>
+        <translation>Windrose</translation>
     </message>
 </context>
 <context>
@@ -67,17 +67,17 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="20"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="32"/>
         <source>Load data from local file (just for developement).</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="25"/>
         <source>Autoupdate</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatisch aktualisieren</translation>
     </message>
 </context>
 <context>
@@ -85,82 +85,82 @@
     <message>
         <location filename="../qml/share/WindRose.qml" line="10"/>
         <source>N</source>
-        <translation type="unfinished"></translation>
+        <translation>N</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="11"/>
         <source>NNE</source>
-        <translation type="unfinished"></translation>
+        <translation>NNO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="12"/>
         <source>NE</source>
-        <translation type="unfinished"></translation>
+        <translation>NO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="13"/>
         <source>ENE</source>
-        <translation type="unfinished"></translation>
+        <translation>ONO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="14"/>
         <source>E</source>
-        <translation type="unfinished"></translation>
+        <translation>O</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="15"/>
         <source>ESE</source>
-        <translation type="unfinished"></translation>
+        <translation>OSO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="16"/>
         <source>SE</source>
-        <translation type="unfinished"></translation>
+        <translation>SO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="17"/>
         <source>SSE</source>
-        <translation type="unfinished"></translation>
+        <translation>SSO</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="18"/>
         <source>S</source>
-        <translation type="unfinished"></translation>
+        <translation>S</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="19"/>
         <source>SSW</source>
-        <translation type="unfinished"></translation>
+        <translation>SSW</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="20"/>
         <source>SW</source>
-        <translation type="unfinished"></translation>
+        <translation>SW</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="21"/>
         <source>WSW</source>
-        <translation type="unfinished"></translation>
+        <translation>WSW</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="22"/>
         <source>W</source>
-        <translation type="unfinished"></translation>
+        <translation>W</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="23"/>
         <source>WNW</source>
-        <translation type="unfinished"></translation>
+        <translation>WNW</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="24"/>
         <source>NW</source>
-        <translation type="unfinished"></translation>
+        <translation>NW</translation>
     </message>
     <message>
         <location filename="../qml/share/WindRose.qml" line="25"/>
         <source>NNW</source>
-        <translation type="unfinished"></translation>
+        <translation>NNW</translation>
     </message>
 </context>
 </TS>
